e2af9c3cb871f9e1edad96422586793ab40407c2
ThekingMX1998 <chen1263754478@gmail.com>
2022-02-27 22:50:28 +0800

    Create 301.js

---
+++ 301.js
@@ -0,0 +1,7 @@
+addEventListener('fetch', event => {
+    event.respondWith(fetchAndApply(event.request));
+})
+async function fetchAndApply(request) {
+    url = 'https://example.com' // 需要跳转到的地址
+    return Response.redirect(url, 301)
+}
