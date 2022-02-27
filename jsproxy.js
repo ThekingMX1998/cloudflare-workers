7ea2599c67d6f761f1561e7aa42f64bdb7925a64
ThekingMX1998 <chen1263754478@gmail.com>
2022-02-27 22:52:16 +0800

    Update and rename jsproxy to jsproxy.js

--- jsproxy.js
+++ jsproxy.js
@@ -3,7 +3,7 @@
 /**
  * static files (404.html, sw.js, conf.js)
  */
-const ASSET_URL = 'https://cdn.free-network.ga'
+const ASSET_URL = 'https://etherdream.github.io/jsproxy'
 
 const JS_VER = 10
 const MAX_RETRY = 1
