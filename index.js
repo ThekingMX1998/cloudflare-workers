c68618caea7530cf527d4388ab763c45782e1ea3
ThekingMX1998 <chen1263754478@gmail.com>
2022-02-27 22:49:04 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const html = `HTML code`
+
+async function handleRequest(request) {
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+    },
+  })
+}
+
+addEventListener("fetch", event => {
+  return event.respondWith(handleRequest(event.request))
+})
